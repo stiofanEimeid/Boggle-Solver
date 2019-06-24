e762190abc8caa73ffa5cc9038c0a12aa62367fd
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2019-06-24 17:58:26 +0000

    new functions and tests added

--- boggle.py
+++ boggle.py
@@ -53,3 +53,40 @@ def all_grid_neighbours(grid):
         position_neighbours = neighbours_of_position(position)
         neighbours[position] = [p for p in position_neighbours if p in grid]
     return neighbours
+    
+def path_to_word(grid, path):
+    """
+    Add all of the letters on the path to a string
+    """
+    return ''.join([grid[p] for p in path])
+    
+def search(grid, dictionary):
+    """
+    Search through the paths to locate words by matching
+    strings to words in a dictionary
+    """
+    neighbours = all_grid_neighbours(grid)
+    paths = []
+    
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if word in dictionary:
+            paths.append(path)
+        for next_pos in neighbours[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+                
+    for position in grid:
+        do_search([position])
+        
+    words = []
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+    
+def get_dictionary(dictionary_file):
+    """
+    Load Dictionary file
+    """
+    with open(dictionary_file) as f:
+        return [w.strip().upper() for w in f]
