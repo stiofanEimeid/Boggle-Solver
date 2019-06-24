ab2522c35d69e5453080a840d03bac76f6c6c631
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2019-06-24 18:15:25 +0000

    boggle solver completed

--- boggle.py
+++ boggle.py
@@ -90,3 +90,21 @@ def get_dictionary(dictionary_file):
     """
     with open(dictionary_file) as f:
         return [w.strip().upper() for w in f]
+
+
+def display_words(words):
+     for word in words:
+        print(word)
+     print("Found %s words" %len(words))
+    
+def main():
+    """
+    This is the function that will run the whole project
+    """
+    grid = make_grid(3, 3)
+    dictionary = get_dictionary('words.txt')
+    words = search(grid, dictionary)
+    display_words(words)
+    
+if __name__ == "__main__":
+    main()
